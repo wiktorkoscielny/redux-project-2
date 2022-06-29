2e970ece701a6f7dee8174e74fde68c69a75ec80
Wiktor Kościelny <81425551+wiktorkoscielny@users.noreply.github.com>
2022-06-29 15:34:12 +0200

    Set up components, created hooks, imported state data

---
+++ src/components/TodoItem.js
@@ -0,0 +1,32 @@
+import React from 'react';
+
+// hooks
+import { useDispatch } from 'react-redux';
+
+// actions
+import { toggleComplete } from '../redux/todoSlice';
+
+const TodoItem = ({ id, title, completed }) => {
+const dispatch = useDispatch();
+
+const handleCompleteClick = () => {
+    dispatch(toggleComplete({id: id, completed: !completed}))
+}
+
+	return (
+		<li className={`list-group-item ${completed && 'list-group-item-success'}`}>
+			<div className='d-flex justify-content-between'>
+				<span className='d-flex align-items-center'>
+					<input type='checkbox' className='mr-3' checked={completed}
+                        onChange={() => handleCompleteClick()}
+                    >
+                    </input>
+					{title}
+				</span>
+				<button className='btn btn-danger'>Delete</button>
+			</div>
+		</li>
+	);
+};
+
+export default TodoItem;
