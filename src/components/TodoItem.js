1af53da403737cff0a4cfdcb5000e9752a30c5ac
Wiktor Kościelny <81425551+wiktorkoscielny@users.noreply.github.com>
2022-06-29 15:37:21 +0200

    Created function to change state with dispatch

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,32 +1,34 @@
-import React from 'react';
+import React from "react";
 
 // hooks
-import { useDispatch } from 'react-redux';
+import { useDispatch } from "react-redux";
 
 // actions
-import { toggleComplete } from '../redux/todoSlice';
+import { toggleComplete } from "../redux/todoSlice";
 
 const TodoItem = ({ id, title, completed }) => {
-const dispatch = useDispatch();
+  const dispatch = useDispatch();
 
-const handleCompleteClick = () => {
-    dispatch(toggleComplete({id: id, completed: !completed}))
-}
+  const handleCompleteClick = () => {
+    dispatch(toggleComplete({ id: id, completed: !completed }));
+  };
 
-	return (
-		<li className={`list-group-item ${completed && 'list-group-item-success'}`}>
-			<div className='d-flex justify-content-between'>
-				<span className='d-flex align-items-center'>
-					<input type='checkbox' className='mr-3' checked={completed}
-                        onChange={() => handleCompleteClick()}
-                    >
-                    </input>
-					{title}
-				</span>
-				<button className='btn btn-danger'>Delete</button>
-			</div>
-		</li>
-	);
+  return (
+    <li className={`list-group-item ${completed && "list-group-item-success"}`}>
+      <div className="d-flex justify-content-between">
+        <span className="d-flex align-items-center">
+          <input
+            type="checkbox"
+            className="mr-3"
+            checked={completed}
+            onChange={() => handleCompleteClick()}
+          ></input>
+          {title}
+        </span>
+        <button className="btn btn-danger">Delete</button>
+      </div>
+    </li>
+  );
 };
 
 export default TodoItem;
